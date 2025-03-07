d564b38ca1d6c774743a10fc9a9d40cff79dbf9b
Roman <92608037+Newtr@users.noreply.github.com>
2025-03-07 15:30:48 +0300

    via upload

--- MyApp/src/main/java/com/example/MyApp/PropertyView/Application/DTO/AddAmenitiesRequest.java
+++ MyApp/src/main/java/com/example/MyApp/PropertyView/Application/DTO/AddAmenitiesRequest.java
@@ -3,8 +3,8 @@ package com.example.MyApp.PropertyView.Application.DTO;
 import java.util.List;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import org.jetbrains.annotations.NotNull;
 
 @Data
 public class AddAmenitiesRequest {
